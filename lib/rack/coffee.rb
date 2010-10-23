0c83b2ce6de6071e31e6da958aead79521153d1e
Jeff Berg <jeff@ministrycentered.com>
2010-10-23 11:32:46 -0700

    Monkey patch Rails:Rack:Static

--- lib/rack/coffee.rb
+++ lib/rack/coffee.rb
@@ -46,3 +46,32 @@ module Rack
     end
   end
 end
+
+module Rails
+  module Rack
+    class Static
+      def call(env)
+        path        = env['PATH_INFO'].chomp('/')
+        method      = env['REQUEST_METHOD']
+        
+        coffee = ::File.join(Rails.root, "public", path.split(".")[0..-2].join(".") + ".coffee")
+        
+        if FILE_METHODS.include?(method) && !::File.exists?(coffee)
+          if file_exist?(path)
+            return @file_server.call(env)
+          else
+            cached_path = directory_exist?(path) ? "#{path}/index" : path
+            cached_path += ::ActionController::Base.page_cache_extension
+
+            if file_exist?(cached_path)
+              env['PATH_INFO'] = cached_path
+              return @file_server.call(env)
+            end
+          end
+        end
+
+        @app.call(env)
+      end
+    end
+  end
+end
